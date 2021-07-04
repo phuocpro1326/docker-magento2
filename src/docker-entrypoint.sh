5ed07168c9363d3a0d1c18e8048202774ff945be
phuocNguyen <phuoc.developerit@gmail.com>
2021-07-04 15:36:11 +0700

    fix get INCUMBENT_USER wrong with macOS

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -15,8 +15,8 @@ if [[ "$UPDATE_UID_GID" = "true" ]]; then
     DOCKER_UID=`stat -c "%u" $MAGENTO_ROOT`
     DOCKER_GID=`stat -c "%g" $MAGENTO_ROOT`
 
-    INCUMBENT_USER=`getent passwd $DOCKER_UID | grep -v www-data | cut -d: -f1`
-    INCUMBENT_GROUP=`getent group $DOCKER_GID | grep -v www-data | cut -d: -f1`
+    INCUMBENT_USER=`getent passwd $DOCKER_UID | cut -d: -f1`
+    INCUMBENT_GROUP=`getent group $DOCKER_GID | cut -d: -f1`
 
     echo "Docker: uid = $DOCKER_UID, gid = $DOCKER_GID"
     echo "Incumbent: user = $INCUMBENT_USER, group = $INCUMBENT_GROUP"
